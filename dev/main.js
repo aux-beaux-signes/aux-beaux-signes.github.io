14d99914c22a555d30a5db51307fb53a8812a23a
Yolanare <y09a3115@gmail.com>
2023-02-24 10:05:29 +0100

    calendar, notices cooler and debug fixes

--- dev/main.js
+++ dev/main.js
@@ -13,6 +13,33 @@ function toggleClass(el, c, state) {
 }
 
 
+// CALENDAR
+const calendarContainer = document.querySelector(".calendar"),
+      calendarMonths = Object.values(calendarContainer.children).reverse();
+
+function selectMonth() {
+    // reset months
+    calendarMonths.forEach((m) =>  { m.classList.remove("active"); })
+
+    // focus corresponding months to secret
+    for (let i = 0; i < parseInt(document.documentElement.getAttribute("secret")); i++) {
+        calendarMonths[i].classList.add("active")
+    }
+}
+
+// clickable months (for later)
+function calendarActivateClicks() {
+    for (let i = 0; i < calendarMonths.length; i++) {
+        calendarMonths[i].addEventListener("click", (cursorEv) => {
+            if (window.location.hash == "#debug") { // only with this hash
+                secretCreate(i + 1);
+            }
+        });
+    }
+}
+calendarActivateClicks();
+
+
 // SECRETS
 // place on the page the secret code
 function secretCreate(secretIndex) {
@@ -22,6 +49,8 @@ function secretCreate(secretIndex) {
           secretData = S.secrets[secretIndex];
 
     secretContainer.innerHTML = secretData.monogram + secretData.code;
+    document.documentElement.setAttribute("secret", secretIndex);
+    selectMonth();
 }
 
 // get the current month
@@ -29,11 +58,14 @@ function getMonth() { const d = new Date(); return d.getMonth() + 1; }
 
 secretCreate(getMonth());
 
-// debug / switch secret by clicking on viewport
-var c = 0;
+// debug / switch secret by clicking on it
 document.querySelector(".secret-container").addEventListener("click", () => {
     if (window.location.hash == "#debug") { // only with this hash
-        console.log("secret n°", c+1); secretCreate(c+1); c = (c < 11) ? c + 1 : 0;
+        var c = parseInt(document.documentElement.getAttribute("secret"));
+        if (c < 12) { c += 1; }
+
+        else { document.documentElement.setAttribute("secret", 1); c = 1; }
+        console.log("secret n°", c); secretCreate(c);
     }
 });
 
@@ -43,9 +75,10 @@ const noticeContainer = document.querySelector(".notice-container"),
       noticeBtn = document.querySelector(".notice-btn");
 
 function noticeCreate() {
-    var noticesHTML = ``;
+    var noticesHTML = ``, delayAdd = 0;
     S.notice.forEach((n) => {
-        noticesHTML += `<div>${n[1]}<span>${n[0]}</span></div>`;
+        noticesHTML += `<div style="--delay-add: ${delayAdd}">${n[1]}<span>${n[0]}</span></div>`;
+        delayAdd += 0.05;
     })
 
     noticeContainer.innerHTML = noticesHTML;
@@ -63,9 +96,11 @@ function noticeScramble() {
         }
     }, 30);
 }
-noticeBtn.addEventListener("click", () => { toggleClass(noticeContainer, "opened") });
-noticeBtn.addEventListener("click", () => { toggleClass(noticeBtn, "active") });
-noticeBtn.addEventListener("click", noticeScramble);
+noticeBtn.addEventListener("click", () => {
+    toggleClass(noticeContainer, "opened");
+    toggleClass(noticeBtn, "active");
+    noticeScramble();
+});
 
 // INFO
 const infoContainer = document.querySelector(".info-container");
